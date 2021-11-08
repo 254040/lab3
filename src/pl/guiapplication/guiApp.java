b368a65cb49544eb73a2b955a9a064b5e8f43c9e
254040 <254040@student.pwr.edu.pl>
2021-11-08 18:37:47 +0100

    Project completed

--- src/pl/guiapplication/guiApp.java
+++ src/pl/guiapplication/guiApp.java
@@ -10,7 +10,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
-import java.util.stream.Collectors;
+
 
 public class guiApp implements ActionListener {
 
